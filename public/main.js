961dc8b44b36553fd9583a221e6361cf3a7e1ce9
sseehh <1s1e1h1@gmail.com>
2014-06-25 01:20:41 -0400

    update

--- public/main.js
+++ public/main.js
@@ -6,6 +6,63 @@ $(document).ready(function() {
 	$('#main-nav').append( mapToggleButton );
 
 
+	function getJSONMeta(e) {
+		var e = $(e);
+		var content = e.find('.post-content');
+		if (!content) return null;
+		
+		var jsonText = content.find('.lang-json').text();
+
+		try {
+			var j = JSON.parse(jsonText);
+			return j;
+		}
+		catch (ex) { }
+		
+		return null;		
+	}
+	
+	
+	//add maps to posts
+	$(window).on('action:topic.loaded', function() {
+		$('.post-row').each(function(index, post) {
+			post = $(post);
+			var json = getJSONMeta(post);
+			if (json) {
+				if (json.where) {
+					
+					var t = $('<div></div>');
+					t.css('width','100%').css('height', '300px');
+
+					var options = {
+						lat: json.where[0],
+						lon: json.where[1]
+					};
+					
+					newMap(t, options, function(map) {
+						post.find('.post-content').after(map);
+					});
+					
+				}
+			}
+			
+		});
+	});
+	
+	//add map chooser to composer
+	$(window).on('action:composer.loaded', function(uuid) {
+		/*
+		<span class="btn btn-link" tabindex="-1">
+		<i class="fa fa-list"></i>
+		</span>
+		*/
+		var whereButton = $('<span class="btn btn-link" tabindex="-1"><i class="fa fa-map-marker"></i></span>');
+		
+		$('.composer .formatting-bar').append(whereButton);
+		
+	});
+	
+	
 	
 	//$(window).on('action:ajaxify.end', function(e, data) {		
 	{
@@ -26,12 +83,24 @@ $(document).ready(function() {
 			contentParent = $('#content').parent();
 			showingMap = true;        
 
-			if (!map)
-				newMap(function(_map) {
+			if (!map) {
+				var t = $('<div></div>');
+				t.css('left', '0')
+				.css('top', '0')
+				.css('width', '100%')
+				.css('height', '100%')
+				.css('position', 'absolute')
+				.css('z-index', '-100')
+				.css('margin-top', '50px')
+				.css('margin-bottom', '-50px');
+
+				
+				newMap(t, { }, function(_map) {
 					map = _map;
 					content = $('#content').detach();
 					contentParent.append(map);    
 				});
+			}
 			else {
 				content = $('#content').detach();
 				contentParent.append(map);
@@ -47,24 +116,23 @@ $(document).ready(function() {
 			showingMap = false;
 		}
 
-		function newMap(cb) {
+		function newMap(target, options, cb) {
 			require([
-				'plugins/nodebb-plugin-map/public/vendor/leaflet/leaflet.js'
+				'/plugins/nodebb-plugin-map/public/vendor/leaflet/leaflet.js'
 			], function(L) {
-				
-				var target = $('<div></div>');//.addClass('container-fluid');
-				target.css('left', '0');
-				target.css('top', '0');
-				target.css('width', '100%');
-				target.css('height', '100%');
-				target.css('position', 'absolute');
-				target.css('z-index', '-100');
-				target.css('margin-top', '50px');
-				target.css('margin-bottom', '-50px');
+				if (!target)
+					target = $('<div></div>');
 								
+				if (!options) {
+					options = {
+						lat: 40,
+						lon: 80
+					};
+				}
+				
 				setTimeout(function() {
 					var initialLocation = {
-						lat: 40, lon: -80
+						lat: options.lat, lon: options.lon
 					};
 					
 					var map = L.map(target[0], {
